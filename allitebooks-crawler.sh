61d301cfba21dd0afaaf5bc51d16382d29c9039a
Jaelson Wagner <jaelson@ciandt.com>
2019-11-12 20:38:39 -0300

    Improvements.

--- allitebooks-crawler.sh
+++ allitebooks-crawler.sh
@@ -23,7 +23,7 @@ debug "SCRIPT_BASE_NAME -> $SCRIPT_BASE_NAME"
 
 
 # allitebooks URL # ====================================================================================================
-ALL_IT_EBOOKS_BASE_URL=http://www.allitebooks.com
+ALL_IT_EBOOKS_BASE_URL=http://www.allitebooks.org
 debug "ALL_IT_EBOOKS_BASE_URL -> $ALL_IT_EBOOKS_BASE_URL"
 # ======================================================================================================================
 
@@ -69,13 +69,13 @@ while true ; do
     case "$1" in
         
         --all|-a)
-            debug "Parameter -> --all|-a" ;;
+            debug "Parameter -> --all|-a"; shift ;;
         
         --category|c)
             debug "Parameter -> --category|-c" ;
             case "$2" in
                 "") log_error "Missing argument category. Please, provide a valid ebook category."; exit 1 ;;
-                *) CATEGORY=$2 ; shift 2 ;;
+                *) EBOOKS_CATEGORY=$2 ; shift 2 ;;
             esac ;;
 
         --) shift ; break ;;
@@ -87,6 +87,8 @@ done
 # ======================================================================================================================
 
 
+
+# Checking mandatory utitlity tools. ===================================================================================
 check_mandatory_utility_tools() {
     local WGET_PATH=`type -p wget`
 
@@ -98,22 +100,29 @@ check_mandatory_utility_tools() {
         }
     fi
 }
+# ======================================================================================================================
 
+
+
+# Download a desired page from allitebooks.org. ========================================================================
 download_html_page() {
-    local URL=$1
-    #([^\/]*)$
-    #[^\/]+(?=\/[^\/]*$)
-    local SUFFIX=`echo $NEXT_PAGE | grep -oP '([^\/]*)$'`
-    local TMP_HTML_PAGE_FILE_PATH=$(mktemp --tmpdir=$TMP_DIR --suffix=.$SUFFIX.html)
+    local PAGE_URL=$1
+    # TODO: handle empty PAGE_URL here.
+    # TODO: method should receive the desired directory to save the downloaded html page
+    #([^\/]*)$              regex extract last part from URL
+    #[^\/]+(?=\/[^\/]*$)    
+    local SUFFIX=`echo $PAGE_URL | grep -oP '([^\/]*)$'`
+    #local TMP_HTML_PAGE_FILE_PATH=$(mktemp --tmpdir=$TMP_HTML_DIR --suffix=.$SUFFIX.html)
+    local TMP_HTML_PAGE_FILE_PATH="$TMP_HTML_DIR/$SUFFIX.html"
 
-    echo -n "Downloading html page $URL..." >&2
+    echo -n "Downloading page $PAGE_URL..." >&2
     
-    wget -nv --show-progress --tries=3 --retry-connrefused \
-    -O "$TMP_HTML_PAGE_FILE_PATH" \
-    "$URL"
+    wget -nv --show-progress --tries=3 --retry-connrefused -O "$TMP_HTML_PAGE_FILE_PATH" "$PAGE_URL"
 
-    echo $TMP_HTML_PAGE_FILE_PATH
+    echo "$TMP_HTML_PAGE_FILE_PATH"
 }
+# ======================================================================================================================
+
 
 download_files_from_catalog() {
     local CATALOG_FILE_PATH=$1
@@ -140,50 +149,64 @@ download_files_from_catalog() {
 }
 
 check_mandatory_utility_tools
-exit 1
 
-# {1,,} to lower case
-EBOOK_LINKS_CATALOG_FILE_PATH="$SCRIPT_PATH/${1,,}-ebook-links-catalog.txt"
-echo $EBOOK_LINKS_CATALOG_FILE_PATH
+CHOOSEN_PAGE_URL="$ALL_IT_EBOOKS_BASE_URL";
+[[ ! -z $EBOOKS_CATEGORY ]] && { CHOOSEN_PAGE_URL="$ALL_IT_EBOOKS_BASE_URL/$EBOOKS_CATEGORY"; }
+log_debug "CHOOSEN_PAGE_URL -> $CHOOSEN_PAGE_URL"
 
-touch $EBOOK_LINKS_CATALOG_FILE_PATH
-cat /dev/null $EBOOK_LINKS_CATALOG_FILE_PATH
-
-
-
-URL_CHOOSEN_CATEGORY_PAGE="$ALL_IT_EBOOKS_BASE_URL/$EBOOK_CATEGORY"
-DOWNLOADED_HTML_PAGE_FILE_PATH=$(download_html_page $URL_CHOOSEN_CATEGORY_PAGE)
+DOWNLOADED_HTML_PAGE_FILE_PATH=$(download_html_page "$CHOOSEN_PAGE_URL")
+log_debug "DOWNLOADED_HTML_PAGE_FILE_PATH -> $DOWNLOADED_HTML_PAGE_FILE_PATH"
 
 PAGES_COUNT=`cat $DOWNLOADED_HTML_PAGE_FILE_PATH | grep -oP '1 / \d+' | grep -oP '\d+$'`
-# PAGES_COUNT=2 # Mock. Remove-me!
+log_debug "PAGES_COUNT -> $PAGES_COUNT"
+PAGES_COUNT=5 # Mock. Remove-me!
 
-log_info "There are $PAGES_COUNT pages for category: $EBOOK_CATEGORY."
+if [[ ! -z $EBOOKS_CATEGORY ]];
+then 
+    { 
+        log_info "$PAGES_COUNT pages in $EBOOKS_CATEGORY category will be processed.";
+        # {variable,,} to lower case
+        EBOOKS_CATALOG_FILE_PATH="$SCRIPT_PATH/${$EBOOKS_CATEGORY,,}-catalog.txt";
+    }
+else
+    {
+        log_info "$PAGES_COUNT pages will be processed.";
+        EBOOKS_CATALOG_FILE_PATH="$SCRIPT_PATH/all-catalog.txt";
+    }
+fi    
 
-for PAGE_NUMBER in `seq 1 $PAGES_COUNT`
-do
-    if [[ $PAGE_NUMBER -gt 1 ]];
+touch $EBOOKS_CATALOG_FILE_PATH
+cat /dev/null > $EBOOKS_CATALOG_FILE_PATH
+
+log_debug "$EBOOKS_CATALOG_FILE_PATH"
+
+if [[ $PAGES_COUNT -gt 0 ]];
     then 
-        { 
-            URL_CHOOSEN_CATEGORY_PAGE="$ALL_IT_EBOOKS_BASE_URL/$EBOOK_CATEGORY/page/$PAGE_NUMBER";
-            DOWNLOADED_HTML_PAGE_FILE_PATH=$(download_html_page $URL_CHOOSEN_CATEGORY_PAGE);
-        }
-    fi
-    
-    declare -a EBOOK_PAGES=()
-    EBOOK_PAGES=($(cat $DOWNLOADED_HTML_PAGE_FILE_PATH | grep -P '<h2 class="entry-title">' | grep -oP 'http:\/\/\S+\w'))
-            
-    log_info "${#EBOOK_PAGES[@]} titles in page $PAGE_NUMBER"
+        {
+            for PAGE_NUMBER in `seq 1 $PAGES_COUNT`
+            do
+                NEXT_PAGE_TO_DOWNLOAD="$CHOOSEN_PAGE_URL/page/$PAGE_NUMBER"
+                DOWNLOADED_HTML_PAGE_FILE_PATH=$(download_html_page $NEXT_PAGE_TO_DOWNLOAD)
 
-    for NEXT_PAGE in "${EBOOK_PAGES[@]}"
-    do
-        HTML_PAGE_FILE_PATH=$(download_html_page $NEXT_PAGE)
-        cat $HTML_PAGE_FILE_PATH | grep -oP 'http:\/\/file[^"]*' >> "$EBOOK_LINKS_CATALOG_FILE_PATH"             
-    done
-done
+                declare -a EBOOK_PAGES=()
+                EBOOK_PAGES=($(cat $DOWNLOADED_HTML_PAGE_FILE_PATH | grep -P '<h2 class="entry-title">' | grep -oP 'http:\/\/\S+\w'))
+                        
+                log_info "Found ${#EBOOK_PAGES[@]} titles in page number: $PAGE_NUMBER."
+
+                for NEXT_PAGE in "${EBOOK_PAGES[@]}"
+                do
+                    log_debug "$NEXT_PAGE"
+                    HTML_PAGE_FILE_PATH=$(download_html_page $NEXT_PAGE)
+                    cat $HTML_PAGE_FILE_PATH | grep -oP 'http:\/\/file[^"]*' >> "$EBOOKS_CATALOG_FILE_PATH"
+                done
+            done
+        }
+fi
+
+
 
 # Download all files from catalog.
-download_files_from_catalog $EBOOK_LINKS_CATALOG_FILE_PATH \
-                            "$EBOOKS_OUTPUT_DIR/$EBOOK_CATEGORY"
+# download_files_from_catalog $EBOOKS_CATALOG_FILE_PATH "$EBOOKS_OUTPUT_DIR/$EBOOKS_CATEGORY"
 
 # Clean up temporary directory.
-# rm -r "$TMP_DIR"
+rm -r "$TMP_HTML_DIR"
